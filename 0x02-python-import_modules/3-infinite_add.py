492e611f8396e2d4b73ebbe05f88c1fec44f01ff
MahmuodAhmaad10 <mahmoudahmaaad3@gmail.com>
2024-01-10 16:09:28 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    suma = 0
-    i = 0
-    for a in sys.argv:
-        if i > 0:
-            suma += int(a)
-        i += 1
-    print(suma)
+import sys
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    total = 0
+    for number in sys.argv:
+        total += int(number)
+    print("{}".format(total))
